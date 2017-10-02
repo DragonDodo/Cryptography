002812e69c3d34992f347aa4c47e33edcb93cf45
DragonDodo <32217699+DragonDodo@users.noreply.github.com>
2017-10-02 16:50:01 +0100

    Moved annealing, made temp global, removed hill climbs

--- pfanneal.py
+++ pfanneal.py
@@ -1,9 +1,10 @@
 # come on, colin, crack the bloody cipher already
-# indeed. 
+# agreed. 
 from random import shuffle, random, choice
 from math import exp, log
 KEY1 = KEY2 = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
 
+T = 1.0 #yes, I know, global variables are dangerous. 
 
 def unpf( ct, key ):
     out = ""
@@ -69,9 +70,9 @@ def handleQuads(fn):
     
 def score(decrypt, quads, cribs = None):
     out = 0.
-    for i in range(len(decrypt)-4):
+    for i in range(len(decrypt)-2):
         try:
-            out += quads[ decrypt[i:i+4] ]
+            out += quads[ decrypt[i:i+2] ]
         except KeyError:
             pass
         if cribs is not None:
@@ -111,38 +112,55 @@ def swapRows(s, i, j):
     
         
 def testKeys(ciphers, k1, k2, quads, best, cribs):
+    
     nc = len ( ciphers )
     ds = map(decrypt, ciphers, [k1 for i in range(nc)], [k2 for i in range(nc)])
     ss = sum(map(score, ds, [quads for i in range(nc)], [cribs for i in range(nc)]))
-    if ss > best[0]:
+    ap = exp((best[0] - ss)/1000*T)
+    if ap > random:
         best = (ss, ds, (k1, k2))
     return best
     
 def climb(k1, k2, ciphers, quads, cribs = None):
     nc = len(ciphers)
-    T = 1.0
+    chooserlist = ['pair', 'cols', 'rows'] #random string chosen from this list to determine what swap
+    global T
+    T = 0.8*T
+    
     ds = map(decrypt, ciphers, [k1 for i in range(nc)], [k2 for i in range(nc)])
     ss = sum(map(score, ds, [quads for i in range(nc)], [cribs for i in range(nc)]))
     best = (ss, ds, (k1, k2))
+    
+    action = choice(chooserlist)
+    print action
+    
     for ii in range(len(k1)):
         for jj in range(ii+1, len(k1)-1):
-            sk1 = swapPair(k1,ii,jj)
-            
-            ap = exp((best[0] - ss)/1000*T)
+            if ii < 5 and jj < 5:
+                if action == 'pair':
+                    sk1 = swapPair(k1,ii,jj)
+                if action == 'cols':
+                    sk1 = swapCols(k1,ii,jj)
+                elif action == 'rows':
+                    sk1 = swapRows(k1,ii,jj)
+                #redo this mess of loops later    
             
             best = testKeys(ciphers, sk1, k2, quads, best, cribs)
-            if ii < 5 and jj < 5 and ap > random():
-                sc1 = swapCols(k1,ii,jj)
-                sr1 = swapRows(k1,ii,jj)
-                sc2 = swapCols(k2,ii,jj)
-                sr2 = swapRows(k2,ii,jj)
-                T = T*0.6
+           # if ii < 5 and jj < 5:
+               # sc1 = swapCols(k1,ii,jj)
+                #sr1 = swapRows(k1,ii,jj)
+               # sc2 = swapCols(k2,ii,jj)
+                #sr2 = swapRows(k2,ii,jj)
                 
+            
                 
-                best = testKeys(ciphers, sc1, k2, quads, best, cribs)                
-                best = testKeys(ciphers, k1, sc2, quads, best, cribs)            
-                best = testKeys(ciphers, sr1, k2, quads, best, cribs)                
-                best = testKeys(ciphers, k1, sr2, quads, best, cribs)            
+               # best = testKeys(ciphers, sc1, k2, quads, best, cribs)                
+                #best = testKeys(ciphers, k1, sc2, quads, best, cribs)            
+               # best = testKeys(ciphers, sr1, k2, quads, best, cribs)                
+                #best = testKeys(ciphers, k1, sr2, quads, best, cribs)   
+                
+            
+                #print T
                 
 # #             for kk in range(len(k2)):
 # #                 for ll in range(kk+1,len(k2)-1):
@@ -184,13 +202,13 @@ def decryptTest():
 # decryptTest()
 
     
-cipher1 = "DUTM UFVD BHWK RHEN RDOY LCVH NRLD QBOR TMKN TVDN TMYH TQSB ECUH LEML RFLK DMNH YEOY COMB NLQS TMKN CDKM NLOC RQBH RCNV HPLE DMNT TWFL RELY QREH SDLN BQEW EDLO QREH DUDN NQOW QREH DCNT"
+cipher1 = "DU UF BH RH RD LC NR QB TM TV TM TQ EC LE RF DM YE CO NL TM CD NL RQ RC HP DM TW RE QR SD BQ ED QR DU NQ QR DC"
 
-cipher2 = "DUTM CBFV RQBH MEOT NKMQ NDMO SFWE HLEK RCUW QREH DMNT RBUE EKRS QREH CWRV EQTY SEPD MNLE ELLU TMKN WBEP RISD GMOS RLEL"
+cipher2 = "DU CB RQ ME NK ND SF HL RC QR DM RB EK QR CW EQ SE MN EL TM WB RI GM RL"
 
-cipher3 = "EOCH TQOL BTOU QREH DVTM NRLF DUTN DRSK MYHT DMNT QKYD BYCV PDUR BOHK RYEL TLOT BDVH BQRY RYBK POFW DELY SEPT LKOM TMKN XREH HWTY NLHZ CPQV BDUH NLQT MDTU DLQH YKED WBEP ALST SQYT RAFW"
+cipher3 = "EO TQ BT QR DV NR DU DR MY DM QK BY PD BO RY TL BD BQ RY PO DE SE LK TM XR HW NL CP BD NL MD DL YK WB AL SQ RA"
 
-quads = handleQuads("english_quadgrams.txt")
+quads = handleQuads("english_bigrams.txt")
 os = -100
 cr = 0
 record = (os, None)
